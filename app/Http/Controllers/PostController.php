f9770d99ed9db354711447263f2471d7d67dfa2a
RahulJaiswalKeenable <rahul_jaiswal@fosteringlinux.com>
2025-05-31 21:22:42 +0530

    first commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index() {
+        return Post::all();
+    }
+
+    public function store(Request $request) {
+        $post = Post::create($request->only(['title', 'description']));
+        return response()->json($post, 201);
+    }
+
+    public function show(Post $post) {
+        return $post;
+    }
+
+    public function update(Request $request, Post $post) {
+        $post->update($request->only(['title', 'description']));
+        return response()->json($post);
+    }
+
+    public function destroy(Post $post)
+{
+    $post->delete();
+    return response()->json(['message' => 'Post deleted successfully'], 200);
+}
+}
+
